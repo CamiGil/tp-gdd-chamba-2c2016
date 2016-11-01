194aaf1664bfaf047afcb0178d1c944b40316c46
Brian Flores Leibenzon <brian.fl@outlook.com>
2016-11-01 20:01:33 -0300

    Arreglos

--- ClinicaFrba/Registro resultado/FormAlta.cs
+++ ClinicaFrba/Registro resultado/FormAlta.cs
@@ -46,8 +46,7 @@ namespace ClinicaFrba.Registro_resultado
                 conexion.Open();
                 SqlCommand guardarAtencion = new SqlCommand("CHAMBA.RegistrarAtencion", conexion);
                 guardarAtencion.CommandType = CommandType.StoredProcedure;
-
-               MessageBox.Show(cboPlan.SelectedValue.ToString());
+                
                 guardarAtencion.Parameters.Add("@IdTurno", SqlDbType.Decimal).Value = decimal.Parse(cboPlan.SelectedValue.ToString());
                 guardarAtencion.Parameters.Add("@Sintomas", SqlDbType.VarChar).Value = textBox1.Text;
                 guardarAtencion.Parameters.Add("@Diagnostico", SqlDbType.VarChar).Value = textBox2.Text;
